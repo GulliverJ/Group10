0516e32295122c17145b5cd6c4c9afa2e1d2f63b
GulliverJ <gully.johnson@gmail.com>
2014-12-16 20:54:07 +0000

    Updated CSS and added 'Site Map' to index.html

--- usSensor/usSensor.ino
+++ usSensor/usSensor.ino
@@ -1,40 +1,34 @@
 const int pingPin = 22;
 byte byteRead = 3;
 long cm = -1;
-int five = 5;
-int initialise = 1;
+int initialise = 1;  // Used to ensure only one reading is sent through
 
 void setup() {
-  // initialize serial communication:
   Serial.begin(9600);
   Serial2.begin(115200);
 }
 
 void loop()
 {
-  // establish variables for duration of the ping, 
-  // and the distance result in inches and centimeters:
-  if( initialise > 0 ) {
+  
+  if( initialise > 0 ) {    // Opens communication with the port
     Serial.println("Start");
     initialise = 0;
   }
     
-  if( cm > 0 ){
-    //Serial2.println(cm);
+  if( cm > 0 ){             // Sends the value only if a reading has been taken.
     Serial.println(cm);
     cm = -1;
   }
-  //Serial.println("test");
-  //delay(500);
+
 }
 
 void serialEvent() {
-  //while (Serial.available()) {
     
     byteRead = Serial.read();
     long duration = 0;
     
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 100; i++) {
       // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
       // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
       pinMode(pingPin, OUTPUT);
@@ -50,10 +44,10 @@ void serialEvent() {
       pinMode(pingPin, INPUT);
       duration += pulseIn(pingPin, HIGH);
     }
-    duration = duration/10;
+    duration = duration/100;
     
     cm = microsecondsToCentimeters(duration);
- // }
+
 }
 
 //Gives a fairly accurate conversion to centimetres
